5d790cd50683af8c4fe6657617a3a43347a4fe5b
ポキ <aman_a@aol.com>
2022-05-23 08:29:38 +0500

    updated command filter

--- scp/core/filters/Command.py
+++ scp/core/filters/Command.py
@@ -1,5 +1,5 @@
 import re
-from typing import List
+from typing import List, Union
 from pyrogram.filters import create
 from pyrogram.types import Message
 from configparser import ConfigParser
@@ -10,50 +10,69 @@ config.read('config.ini')
 prefixes = config.get('scp-5170', 'prefixes').split()
 
 
-def command(
-    commands: str or List[str],
-    prefixes: str or List[str] = prefixes,
-    case_sensitive: bool = False,
-):
+def command(commands: Union[str, List[str]], prefixes: Union[str, List[str]] = prefixes, case_sensitive: bool = False):
+    """Filter commands, i.e.: text messages starting with "/" or any other custom prefix.
+    Parameters:
+        commands (``str`` | ``list``):
+            The command or list of commands as string the filter should look for.
+            Examples: "start", ["start", "help", "settings"]. When a message text containing
+            a command arrives, the command itself and its arguments will be stored in the *command*
+            field of the :obj:`~pyrogram.types.Message`.
+        prefixes (``str`` | ``list``, *optional*):
+            A prefix or a list of prefixes as string the filter should look for.
+            Defaults to "/" (slash). Examples: ".", "!", ["/", "!", "."], list(".:!").
+            Pass None or "" (empty string) to allow commands with no prefix at all.
+        case_sensitive (``bool``, *optional*):
+            Pass True if you want your command(s) to be case sensitive. Defaults to False.
+            Examples: when True, command="Start" would trigger /Start but not /start.
     """
-    This is a drop in replacement for the
-    default filters.command that is included
-    in Pyrogram. The Pyrogram one does not support
-    /command@botname type commands,
-    so this custom filter enables that throughout
-    all groups and private chats.
-    This filter works exactly the same as the original
-    command filter even with support for multiple command
-    prefixes and case sensitivity.
-    Command arguments are given to user as message.command
-    """
+    command_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
 
     async def func(flt, client: core.clients.Client, message: Message):
-        text: str = message.text or message.caption
+        username = client.me.username or ""
+        text = message.text or message.caption
         message.command = None
+
         if not text:
             return False
-        regex = r'(?i)^({prefix})({regex})(@{bot_name})?(\s.*)?$'.format(
-            prefix='|'.join(re.escape(x) for x in flt.prefixes),
-            regex='|'.join(flt.commands),
-            bot_name=client.me.username.lower(
-            ) if client.me.username else None,
-        )
-        if matches := re.search(regex, text, flags=re.IGNORECASE):
-            message.command = [matches[2]]
-            if matches[4]:  # The command has arguments
-                message.command.extend(list(matches[4].strip().split()))
-            return True
+
+        for prefix in flt.prefixes:
+            if not text.startswith(prefix):
+                continue
+
+            without_prefix = text[len(prefix):]
+
+            for cmd in flt.commands:
+                if not re.match(rf"^(?:{cmd}(?:@?{username})?)(?:\s|$)", without_prefix, flags=0 if flt.case_sensitive else re.IGNORECASE):
+                    continue
+
+                without_command = re.sub(rf"{cmd}(?:@?{username})?\s?", "", without_prefix, count=1, flags=0 if flt.case_sensitive else re.IGNORECASE)
+
+
+                # match.groups are 1-indexed, group(1) is the quote, group(2) is the text
+                # between the quotes, group(3) is unquoted, whitespace-split text
+
+                # Remove the escape character from the arguments
+                message.command = [cmd] + [
+                    re.sub(r"\\([\"'])", r"\1", m.group(2) or m.group(3) or "")
+                    for m in command_re.finditer(without_command)
+                ]
+
+                return True
+
         return False
+
     commands = commands if isinstance(commands, list) else [commands]
     commands = {c if case_sensitive else c.lower() for c in commands}
-    prefixes = prefixes or []
+
+    prefixes = [] if prefixes is None else prefixes
     prefixes = prefixes if isinstance(prefixes, list) else [prefixes]
-    prefixes = set(prefixes) if prefixes else {''}
+    prefixes = set(prefixes) if prefixes else {""}
+
     return create(
         func,
-        'CustomUserCommandFilter',
+        "CommandFilter",
         commands=commands,
         prefixes=prefixes,
-        case_sensitive=case_sensitive,
+        case_sensitive=case_sensitive
     )
